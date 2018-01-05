cd87fbfa2a11958833f8d423272eac3466eb8702
Krzysztof <musashimiyamoto@interia.eu>
2018-01-05 13:48:27 +0100

    mod_1zad_2

---
+++ app/program.js
@@ -0,0 +1,39 @@
+﻿process.stdin.setEncoding('utf-8');
+var OSinfo = require('../modules/OSinfo');
+var timeFormat = require('../modules/timeFormat');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+
+            case '/ver':
+                console.log(process.versions);
+                break;
+
+            case '/tmp':
+                var tmpdir = os.tmpdir();
+                console.log(tmpdir);
+                break;
+
+            case '/getOSinfo':
+                OSinfo.print();
+                timeFormat.format();
+                break;
+                
+            case '/time':                       
+                timeFormat.format(5000);
+                break;
+            
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        };
+    }
+});
---
+++ modules/timeFormat.js
@@ -0,0 +1,12 @@
+﻿var os = require('os');
+
+function timeFormat(changeToTime) {
+        var uptime = os.uptime();
+        var hours = (uptime / 3600).toFixed(0);
+        var minutes = (uptime % 3600 / 60).toFixed(0);
+        var seconds = (uptime % 60).toFixed(0);
+        
+        console.log(hours + 'h ' + minutes + 'm ' + seconds + 's');
+    }
+
+exports.format = timeFormat;
